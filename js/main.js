a467aed49513c02474d1b5316214db10e7d39b1d
saadouneimad <imade760@gmail.com>
2021-08-24 09:42:49 +0100

    fac and shaky button

--- js/main.js
+++ js/main.js
@@ -26,6 +26,31 @@
 		});
 	});
 
+ /*=========================================================================
+	shaky button
+=========================================================================*/ 
+
+    $( document ).ready(function() {
+        setInterval(function() {
+         var div = document.getElementsByClassName('btn-shaky');
+         var interval = 100;
+         var distance = 500;
+         var times = 6;
+   
+         $(div).css('position', 'relative');
+   
+         for (var iter = 0; iter < (times + 1) ; iter++) {
+             $(div).animate({
+                 left: ((iter % 2 == 0 ? distance : distance * -1))
+             }, interval);
+         }                                                                                                          
+         $(div).animate({ left: 0 }, interval);
+
+                                                                                                        
+       
+
+        },5000);
+  });
 /*=========================================================================
         Mobile Menu
 =========================================================================*/  
